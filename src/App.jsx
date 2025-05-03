e44819bb9ee4ecdd2dab04de96c839e6c22ea26b
diegoepic <diegoepic1234@hotmail.com>
2025-05-02 23:38:12 -0400

    importacion en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom' ;
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Cart from "./components/cart";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div className="min-vh-100 d-flex flex-column bg-light">
+
       <Navbar />
-      <Cart />
+      <Home />
+      <Cart />  
       <Footer />
     </div>
   );
